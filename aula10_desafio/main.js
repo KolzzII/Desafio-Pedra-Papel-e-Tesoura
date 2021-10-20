dff4f291afd93ff773d2bb38866b461b8640dce8
Junior <86266735+KolzzII@users.noreply.github.com>
2021-10-19 21:36:01 -0300

    Adição do placar e botão reset.

--- aula10_desafio/main.js
+++ aula10_desafio/main.js
@@ -1,4 +1,6 @@
 var rps = 0;
+var voce = 0;
+var junin = 0;
 
 function pPedra(){
     rps = Math.floor(Math.random() * (4 - 1) +1);
@@ -7,19 +9,20 @@ function pPedra(){
     }
     else if (rps == 2){
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Pedra X Junin escolheu Papel</h3> <h3>Você perdeu! ;-; </h3>'
-
+        junin ++;
     } 
     else{
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Pedra X Junin escolheu Tesoura </h3> <h3>Você venceu! :D </h3>'
-
+        voce ++;
     }
+    document.querySelector(".placar").innerHTML = `${voce} X ${junin}`
 }
 
 function pPapel(){
     rps = Math.floor(Math.random() * (4 - 1) +1);
     if(rps == 1){
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Papel X Junin escolheu Pedra </h3><h3> Você venceu! :D </h3>'
-
+        voce ++;
     }
     else if (rps == 2){
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Papel X Junin escolheu Papel </h3><h3> Empatou! :| </h3>'
@@ -27,20 +30,31 @@ function pPapel(){
     } 
     else{
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Papel X Junin escolheu Tesoura </h3><h3> Você perdeu! ;-; </h3>'
-
+        junin ++;
     }
+    document.querySelector(".placar").innerHTML = `${voce} X ${junin}`
 }
 
 function pTesoura(){
     rps = Math.floor(Math.random() * (4 - 1) +1);
     if(rps == 1){
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Tesoura X Junin escolheu Pedra </h3><h3> Você perdeu! ;-; </h3>'
-
+        junin ++;
     }
     else if (rps == 2){
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Tesoura X Junin escolheu Papel </h3><h3> Você venceu! :D </h3>'
+        voce ++;
     } 
     else{
         document.querySelector(".resultado").innerHTML = '<h3>Você escolheu Tesoura X Junin escolheu Tesoura </h3><h3> Empatou! :| </h3>'
     }
+    document.querySelector(".placar").innerHTML = `${voce} X ${junin}`
+}
+
+function reset() {
+    voce = 0;
+    junin = 0;
+    document.querySelector(".placar").innerHTML = `${voce} X ${junin}`
+    document.querySelector(".resultado").innerHTML = '<h3></h3>'
+
 }
